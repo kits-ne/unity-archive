9206676cdf06e5433f4d407d73af8d400a661760
aaron <qkrsogusl3@gmail.com>
2023-07-24 22:57:24 +0900

    feat(2d-controller): init sample

---
+++ Assets/Samples/RigidBodyController/ControllerSettings.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Samples.RigidBodyController
+{
+    [CreateAssetMenu(fileName = "Controller Settings", menuName = "2dController", order = 0)]
+    public class ControllerSettings : ScriptableObject
+    {
+        
+    }
+}
